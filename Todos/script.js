574a33825ebb7858219777f74c87b5b249b22d93
kbu715 <kbu715@gmail.com>
2020-12-31 00:36:56 +0900

    ls setting

--- Todos/script.js
+++ Todos/script.js
@@ -2,6 +2,14 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todosUL = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -23,15 +31,34 @@ function addTodo(todo) {
 
     todoEl.addEventListener('click', () => {
       todoEl.classList.toggle('completed');
+      updateLS();
     });
 
     todoEl.addEventListener('contextmenu', (e) => {
       e.preventDefault();
 
       todoEl.remove();
+      updateLS();
     });
 
     todosUL.appendChild(todoEl);
     input.value = '';
+
+    updateLS();
   }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll('li');
+
+  const todos = [];
+
+  todosEl.forEach((todo) => {
+    todos.push({
+      text: todo.innerText,
+      completed: todo.classList.contains('completed'),
+    });
+  });
+
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
