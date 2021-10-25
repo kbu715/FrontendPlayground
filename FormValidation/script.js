2d17b9043778d176cd484dc387d3a0f90bfc6d1b
kbu715 <kbu715@gmail.com>
2021-10-25 22:32:58 +0900

    ✨ Update: Realtime Form Validation (using 'keyup' event)

--- FormValidation/script.js
+++ FormValidation/script.js
@@ -1,14 +1,62 @@
 const form = document.getElementById('form');
+const inputs = document.querySelectorAll('input');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+inputs.forEach(input => {
+	input.addEventListener('keyup', (e) => {
+		checkInput(e.currentTarget.id);
+	})
+})
+
 form.addEventListener('submit', e => {
 	e.preventDefault();
-	
 	checkInputs();
 });
+
+function checkInput(id) {
+	console.log(id);
+	if(id === 'username') {
+		const usernameValue = username.value.trim();
+		if(usernameValue === '') {
+			setErrorFor(username, 'Username cannot be blank');
+		} else {
+			setSuccessFor(username);
+		}
+	}
+
+	if(id === 'email') {
+		const emailValue = email.value.trim();
+		if(emailValue === '') {
+			setErrorFor(email, 'Email cannot be blank');
+		} else if (!isEmail(emailValue)) {
+			setErrorFor(email, 'Not a valid email');
+		} else {
+			setSuccessFor(email);
+		}
+	} 
+
+	if(id === 'password' || id === 'password2') {
+		const passwordValue = password.value.trim();
+		const password2Value = password2.value.trim();
+
+		if(passwordValue === '') {
+			setErrorFor(password, 'Password cannot be blank');
+		} else {
+			setSuccessFor(password);
+		}
+
+		if(password2Value === '') {
+			setErrorFor(password2, 'Password confirm cannot be blank');
+		} else if(passwordValue !== password2Value) {
+			setErrorFor(password2, 'Passwords does not match');
+		} else{
+			setSuccessFor(password2);
+		}
+	}
+}
 
 function checkInputs() {
 	// trim to remove the whitespaces
