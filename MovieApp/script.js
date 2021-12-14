ce8fa6ba83c4bcc0b13b10b939f642190689f416
kbu715 <kbu715@gmail.com>
2021-12-14 22:05:47 +0900

    feat: movie list sorting

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -7,6 +7,11 @@ const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
+// store
+const store = {
+    movies: []
+}
+
 // Get initial movies
 getMovies(API_URL)
 
@@ -14,14 +19,33 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    showMovies(data.results)
+    let result
+    if(data.results.length === 0) {
+        result = []
+    } else {
+        result = data.results
+    }
+    store.movies = result
+    showMovies(result)
 }
 
 function showMovies(movies) {
     main.innerHTML = ''
 
+    if(movies.length === 0) {
+        main.innerHTML = `
+            <div class="wrapper">
+                <h1 class="text">Not founds 🎃</h1>
+            </div>
+        `
+    }
+
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview } = movie
+        const { title, poster_path, vote_average, overview, popularity, release_date } = movie
+
+        // console.log(new Date(release_date).getTime()) // 최신순 비교 데이터
+
+        // console.log(Math.floor(popularity)) // 인기순
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
@@ -31,8 +55,10 @@ function showMovies(movies) {
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                <span class="popularity">${Math.floor(popularity / 100)}</span>
             </div>
             <div class="overview">
+                <span>${release_date}</span>
                 <h3>Overview</h3>
                 ${overview}
                 </div>
@@ -48,6 +74,38 @@ function getClassByRate(vote) {
         return 'orange'
     } else {
         return 'red'
+    }
+}
+
+
+// select onChange
+function handleChange(selectedValue) {
+
+    console.log('check')
+
+    let temp;
+
+    if(selectedValue === 'popularity') {
+        main.innerHTML = ''
+        temp = [
+            ...store.movies
+        ]
+        showMovies(temp.sort((a, b) => b.popularity - a.popularity))
+    } else if(selectedValue === 'latest') {
+        main.innerHTML = ''
+        temp = [
+            ...store.movies
+        ]
+        showMovies(temp.sort((a, b) => new Date(b.release_date).getTime() - new Date(a.release_date).getTime()))
+    } else if(selectedValue === 'vote_average') {
+        main.innerHTML = ''
+        temp = [
+            ...store.movies
+        ]
+        showMovies(temp.sort((a, b) => b.vote_average - a.vote_average))
+    } else {
+        main.innerHTML = ''
+        showMovies(store.movies)
     }
 }
 
